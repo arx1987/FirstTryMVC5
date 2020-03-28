acec6336d541191dd012a902fa2a4cb4a07de75d
arx1987 <alexandrisaev1987@gmail.com>
2020-03-28 12:43:00 +0200

    Добавьте файлы проекта.

---
+++ Models/DBInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace FirstTryMVC5.Models {
+  public class DBInitializer : DropCreateDatabaseAlways<TestContext> {
+    protected override void Seed(TestContext context) {
+      context.TestsList.Add(new QuestionAnswer() { Subject = "1-ая тема", Question = "первый вопрос", Answer = "правильный ответ на первый вопрос", AskAmount = 0, RightAnsAmount = 0 });
+      context.TestsList.Add(new QuestionAnswer() { Subject = "1-ая тема", Question = "второй вопрос", Answer = "правильный ответ на второй вопрос", AskAmount = 0, RightAnsAmount = 0 });
+      context.TestsList.Add(new QuestionAnswer() { Subject = "1-ая тема", Question = "третий вопрос", Answer = "правильный ответ на третий вопрос", AskAmount = 0, RightAnsAmount = 0 });
+      context.TestsList.Add(new QuestionAnswer() { Subject = "2-ая тема", Question = "первый вопрос", Answer = "правильный ответ на первый вопрос", AskAmount = 0, RightAnsAmount = 0 });
+      context.TestsList.Add(new QuestionAnswer() { Subject = "2-ая тема", Question = "Второй вопрос", Answer = "правильный ответ на первый вопрос", AskAmount = 0, RightAnsAmount = 0 });
+      context.TestsList.Add(new QuestionAnswer() { Subject = "2-ая тема", Question = "третий вопрос", Answer = "правильный ответ на третий вопрос", AskAmount = 0, RightAnsAmount = 0 });
+      context.TestsList.Add(new QuestionAnswer() { Subject = "3-я тема", Question = "первый вопрос", Answer = "правильный ответ на первый вопрос", AskAmount = 0, RightAnsAmount = 0 });
+      context.TestsList.Add(new QuestionAnswer() { Subject = "3-я тема", Question = "второй вопрос", Answer = "правильный ответ на второй вопрос", AskAmount = 0, RightAnsAmount = 0 });
+      context.TestsList.Add(new QuestionAnswer() { Subject = "3-я тема", Question = "третий вопрос", Answer = "правильный ответ на третий вопрос", AskAmount = 0, RightAnsAmount = 0 });
+      base.Seed(context);
+    }
+  }
+}
---
+++ Models/QuestionAnswer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstTryMVC5.Models {
+  public class QuestionAnswer {
+    public int Id { get; set; }
+    public string Subject { get; set; }
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    public int AskAmount { get; set; }
+    public int RightAnsAmount { get; set; }
+  }
+}
